0651f501e2e0d031669f3d6ee53f6709a6147d66
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-09-06 14:30:11 +0300

    finish add notes bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -12,14 +12,15 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                12,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
               ),
             ),
             context: (context),
             builder: (context) {
-              return AddNoteBottomSheet();
+              return const AddNoteBottomSheet();
             },
           );
         },
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/components.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,16 +9,32 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.only(
-          top: 24,
-          left: 16,
-          right: 16,
-        ),
-        child: Column(
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 24,
+        left: 16,
+        right: 16,
+      ),
+      child: Container(
+        child: const Column(
           children: [
-            CustomTextField(),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
           ],
         ),
       ),
