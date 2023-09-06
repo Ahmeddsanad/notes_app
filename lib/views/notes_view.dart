3904d2ae642f2909bfbbe3048c14e40a97b1fc7b
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-09-06 13:25:08 +0300

    add border radius to bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/news_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,9 +10,65 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       body: NewsViewBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                12,
+              ),
+            ),
+            context: (context),
+            builder: (context) {
+              return AddNoteBottomSheet();
+            },
+          );
+        },
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+
+
+// class Popover extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final theme = Theme.of(context);
+
+//     return Container(
+//       margin: const EdgeInsets.all(16.0),
+//       clipBehavior: Clip.antiAlias,
+//       decoration: BoxDecoration(
+//         color: theme.cardColor,
+//         borderRadius: const BorderRadius.all(Radius.circular(32.0)),
+//       ),
+//       child: Column(
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           _buildHandle(context),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildHandle(BuildContext context) {
+//     final theme = Theme.of(context);
+
+//     return FractionallySizedBox(
+//       widthFactor: 0.25,
+//       child: Container(
+//         margin: const EdgeInsets.symmetric(
+//           vertical: 12.0,
+//         ),
+//         child: Container(
+//           height: 5.0,
+//           decoration: BoxDecoration(
+//             color: theme.dividerColor,
+//             borderRadius: const BorderRadius.all(Radius.circular(2.5)),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
